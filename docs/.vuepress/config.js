9b2faa91f286196c0a31b4e97932a52ecb93d95c
Ryan Acevedo <pair+ryan@spacecamp.ninja>
2021-02-14 15:14:20 -0700

    removed redudant nav and replaced with theme nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  title: "Ryan Acevedo",
+  themeConfig: {
+    nav: [
+      {text: 'Home', link: '/'},
+      {text: 'About', link: '/about/'},
+      {text: 'Blog', link: '/blog/'},
+      {text: 'Contact', link: '/contact/'},
+    ]
+  },
   plugins: [
     [
       'vuepress-plugin-typescript',
