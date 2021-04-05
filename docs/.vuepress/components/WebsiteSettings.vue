98fa512674a9e1314ff30da11ad4f5cde0ce55d3
Ryan Acevedo <pair+ryan@spacecamp.ninja>
2021-04-04 18:18:38 -0600

    added button for toggling settings

--- docs/.vuepress/components/WebsiteSettings.vue
+++ docs/.vuepress/components/WebsiteSettings.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-      <button class="absolute right-0 bottom-0 outline">Hiiiiii</button>
+    <button class="absolute right-0 bottom-0 outline">
+      <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      >
+        <path
+          d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 
